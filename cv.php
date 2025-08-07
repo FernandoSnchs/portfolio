e553417dee733fac9d117af3d839f9e8952965cd
FernandoSnchs <fernando.sanches.rodrigues@outlook.com>
2025-08-07 14:27:55 -0300

    changes

---
+++ cv.php
@@ -0,0 +1,88 @@
+<?php
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Curriculum Vitae|Fernando Sanches</title>
+        <link rel="stylesheet" href="cv.css">
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    </head>
+    <body class="cv_body">
+            <ul class="menu">
+                <li><a href="index.html">voltar</a></li>
+            </ul>
+            <div class="container">
+                <div class="top">
+                    <div class="imgBx">
+                        <div class="box">
+                            <img src="imgs/20250725_224449312_iOS.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="profileText">
+                        <h3>Fernando<br>Sanches<br><span>Developer FullStack</span></h3>
+                    </div>
+                </div>
+                <div class="contentBox">
+                    <div class="leftSide">
+                        <h3>Contact Info</h3>
+                        <ul>
+                            <li>
+                                <span class="icon"><ion-icon name="mail-outline"></ion-icon></span>
+                                <span class="text">fernando.sanches.rodrigues@outlook.com</span>
+                            </li>
+                            <li>
+                                <span class="icon"><ion-icon name="logo-instagram"></ion-icon></span>
+                                <span class="text">@sancheswz</span>
+                            </li>
+                            <li>
+                                <span class="icon"><ion-icon name="compass-outline"></ion-icon></span>
+                                <span class="text">https://fernandosnchs.github.io/portfolio/</span>
+                            </li>
+                            <li>
+                                <span class="icon"><ion-icon name="location-outline"></ion-icon></span>
+                                <span class="text">São Caetano do Sul, São Paulo, Brasil</span>
+                            </li>
+                        </ul>
+                        <h3>Education</h3>
+                        <ul class="education">
+                            <li>
+                                <h5>2024 - Present</h5>
+                                <h4>Técnico em Desenvolvimento de Sistemas</h4>
+                                <h6>Escola SENAI "Armando de Arruda Pereira" </h6>
+                            </li>
+                        </ul>
+                        <h3>Language</h3>
+                        <ul class="language">
+                            <li>
+                                <span class="text">English</span>
+                                <span class="percent">
+                                    <div style="width: 80%;"></div> <!-- 800px era exagerado -->
+                                </span>
+                            </li>
+                            
+                            <li>
+                                <span class="text">Portuguese</span>
+                                <span class="percent"><div style="width: 100%;"></div></span>
+                            </li>
+                            
+                            <li>
+                                <span class="text">Spanish</span>
+                                <span class="percent"><div style="width: 50%;"></div></span>
+                            </li>
+                            
+                            <li>
+                                <span class="text">French</span>
+                                <span class="percent"><div style="width: 10%;"></div></span>
+                            </li>
+                        </ul>
+                        
+                    </div>
+                    <div class="rightSide"></div>
+                </div>
+            </div>
+            <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    </body>
+    </html>
+?>
